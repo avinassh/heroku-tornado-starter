18ee53a3875aebf2aa5edf7e30892a1a0cae44d6
avinassh <avinashsajjan@gmail.com>
2013-12-16 17:54:40 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#! /bin/python
+
+import os.path 
+
+# tornado imports
+import tornado.httpserver 
+import tornado.ioloop 
+import tornado.options 
+import tornado.web
+
+# define a port for serving requests. Heroku uses 5000
+from tornado.options import define, options
+define("port", default=5000, help="run on the given port", type=int)
+
+if __name__ == '__main__': 
+    tornado.options.parse_command_line() 
+    app = tornado.web.Application(
+        handlers=[(r'/', IndexHandler)], 
+        # if you want to use any templates, uncomment the below line
+        # template_path=os.path.join(os.path.dirname(__file__), "templates"), 
+        static_path=os.path.join(os.path.dirname(__file__), "static"), 
+        debug=True
+        )
+    # set debug to False when running on production/Heroku!
+    http_server = tornado.httpserver.HTTPServer(app) 
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()        
